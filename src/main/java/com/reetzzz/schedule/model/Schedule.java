54d3d17b9756ab4f09672d541577cef76713acaf
JenniferReetz <jenniferheloisareetz@gmail.com>
2025-03-18 22:19:28 -0300

    Estruturando a model schedule

--- src/main/java/com/reetzzz/schedule/model/Schedule.java
+++ src/main/java/com/reetzzz/schedule/model/Schedule.java
@@ -1,3 +1,29 @@
 package com.reetzzz.schedule.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Schedule {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String weekday;
+
+    private LocalTime time;
 }
