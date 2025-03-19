7fd9695430609f98376fdc54ce6f7e874d419a1e
JenniferReetz <jenniferheloisareetz@gmail.com>
2025-03-18 22:21:52 -0300

    Estruturando a schedule repository

--- src/main/java/com/reetzzz/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/reetzzz/schedule/repository/ScheduleRepository.java
@@ -1,4 +1,10 @@
 package com.reetzzz.schedule.repository;
 
-public class ScheduleRepository {
+import com.reetzzz.schedule.model.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+    List<Schedule> findByWeekday(String weekday);
 }
