7dc0edfd07cde3519c1796321b700311a07082ac
StefanieReetz <stefaniebeatrizreetz@gmail.com>
2025-04-05 00:01:05 -0300

    Tirando as coisas que nao funcionavam

--- src/main/java/com/reetzzz/schedule/controller/ScheduleController.java
+++ src/main/java/com/reetzzz/schedule/controller/ScheduleController.java
@@ -1,13 +1,9 @@
 package com.reetzzz.schedule.controller;
 import java.util.List;
-import java.util.Optional;
 
 import com.reetzzz.schedule.DTO.GradeDTO;
-import com.reetzzz.schedule.model.Schedule;
-import com.reetzzz.schedule.model.Weekday;
 import com.reetzzz.schedule.service.ScheduleService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -17,16 +13,6 @@ public class ScheduleController {
     @Autowired
     private ScheduleService service;
 
-    @GetMapping
-    public List<Schedule> getAllSchedules() {
-        return service.getAllSchedules();
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Schedule> getScheduleById(@PathVariable Long id) {
-        Optional<Schedule> schedule = service.getScheduleById(id);
-        return schedule.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
-    }
     @GetMapping("/grades")
     public ResponseEntity<List<GradeDTO>> getGradesFromGradeApi() {
         try {
@@ -39,33 +25,9 @@ public class ScheduleController {
 
         }
     }
-    @GetMapping("/weekday/{weekday}")
-    public List<Schedule> getSchedulesByWeekday(@PathVariable Weekday weekday) {
-        return service.getSchedulesByWeekday(weekday);
-    }
 
-    @PostMapping
-    public ResponseEntity<Schedule> createSchedule(@RequestBody Schedule schedule) {
-        Schedule savedSchedule = service.saveSchedule(schedule);
-        return new ResponseEntity<>(savedSchedule, HttpStatus.CREATED);
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<Schedule> updateSchedule(@PathVariable Long id, @RequestBody Schedule newSchedule) {
-        Optional<Schedule> schedule = service.getScheduleById(id);
-        if (schedule.isPresent()) {
-            Schedule updatedSchedule = schedule.get();
-            updatedSchedule.setName(newSchedule.getName());
-            updatedSchedule.setWeekdays(newSchedule.getWeekdays());
-            updatedSchedule.setTime(newSchedule.getTime());
-            return ResponseEntity.ok(service.saveSchedule(updatedSchedule));
-        }
-        return ResponseEntity.notFound().build();
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteSchedule(@PathVariable Long id) {
-        service.deleteSchedule(id);
-        return ResponseEntity.noContent().build();
-    }
 }
+
+
+
+
--- src/main/java/com/reetzzz/schedule/service/ScheduleService.java
+++ src/main/java/com/reetzzz/schedule/service/ScheduleService.java
@@ -1,42 +1,19 @@
 package com.reetzzz.schedule.service;
 import com.reetzzz.schedule.DTO.GradeDTO;
-import com.reetzzz.schedule.model.Schedule;
-import com.reetzzz.schedule.model.Weekday;
-import com.reetzzz.schedule.repository.ScheduleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class ScheduleService {
-    @Autowired
-    private ScheduleRepository repository;
+
     private final RestTemplate restTemplate = new RestTemplate();
     private final String GRADE_API_URL = "http://localhost:3002/api/grades";
-    public List<Schedule> getAllSchedules() {
-        return repository.findAll();
-    }
     public List<GradeDTO> getGradesFromGradeApi() {
         GradeDTO[] grades = restTemplate.getForObject(GRADE_API_URL, GradeDTO[].class);
         return grades != null ? Arrays.asList(grades) : List.of();
-    }
-    public Optional<Schedule> getScheduleById(Long id) {
-        return repository.findById(id);
-    }
-
-    public List<Schedule> getSchedulesByWeekday(Weekday weekdays) {
-        return repository.findByWeekdays(weekdays);
-    }
-
-    public Schedule saveSchedule(Schedule schedule) {
-        return repository.save(schedule);
-    }
-
-    public void deleteSchedule(Long id) {
-        repository.deleteById(id);
     }
 }
